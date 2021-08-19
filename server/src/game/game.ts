5a67eedf73a748799e0a05d4c662263cfa7496df
Andy <andyqin17@gmail.com>
2021-08-19 15:24:18 -0700

    Remove HandlerManager and tests

--- server/src/game/game.ts
+++ server/src/game/game.ts
@@ -1,19 +1,18 @@
 import { PlayerManager } from "./playerManager";
 import { nanoid } from "nanoid";
-import { HandlerManager } from "./handlerManager";
 
 export class Game {
   playerManager: PlayerManager;
-  handlerManager: HandlerManager;
   name: string;
   id: string;
 
   constructor(name: string) {
     this.name = name;
     this.playerManager = new PlayerManager();
-    this.handlerManager = new HandlerManager(this);
     this.id = nanoid();
   }
+
+
 
   close() {
     this.playerManager.close();
--- server/src/types/types.ts
+++ server/src/types/types.ts
@@ -1,11 +1,3 @@
 
 import { Game } from "../game/game";
 import { Player } from "../game/player";
-
-
-export interface Handler {
-  handlerFactory: (game: Game, player?: Player) => {
-    (...args: any): void;
-  },
-  eventName: string;
-}
