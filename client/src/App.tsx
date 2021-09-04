f72f2e756d4a45f61f6ecb8634f73951177c8a3c
Andy <andyqin17@gmail.com>
2021-09-04 14:33:31 -0700

    Add client dependencies

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Col, Row, Container } from 'react-bootstrap';
 import './App.css';
+import './bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className='d-flex align-items-center'>
+      <Row className='w-100 align-items-center'>
+        <Col className='text-center'>
+          <h1>Hello World!</h1>
+        </Col>
+      </Row>
+    </Container>
   );
 }
-
 export default App;
